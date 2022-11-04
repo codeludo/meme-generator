6b4708822e2bd2212f9f17f528313df7e16e78a0
camilo <camiloacevedomoreno@gmail.com>
2022-11-04 18:12:36 -0500

    useState to change meme images

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,16 +1,16 @@
 import "../style/components/meme.css"
-import React from "react"
+import React, {useState} from "react"
 import data from "../utils/data"
 
 const Meme = () => {
-
+	const [memeImage, setMemeImage] = useState("https://i.imgflip.com/46y0ne.png")
     
     function memesImage() {
         const memesArray = data.data.memes
         const randNum = Math.floor(Math.random()*memesArray.length-1)
         const imageUrl = memesArray[randNum].url
         console.log("image: " + imageUrl)
-        return imageUrl
+		setMemeImage(imageUrl)
     }
 
 
@@ -31,7 +31,7 @@ const Meme = () => {
 				</div>
 				<button onClick={memesImage} className="btn-image">Get a new meme image 🖼</button>
                 <figure>
-                    <img className="image" src="https://i.imgflip.com/46y0ne.png" alt="meme" />
+                    <img className="image" src={memeImage} alt="meme" />
                 </figure>
 			</div>
 		</main>
